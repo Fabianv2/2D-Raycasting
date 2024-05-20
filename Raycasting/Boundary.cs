90e8a038770baf25ebc69c96ea2f996ad4d39969
Fabianv2 <fabian.kittelberger@gmx.de>
2024-05-20 10:08:40 +0200

    Creating boundaries

---
+++ Raycasting/Boundary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Raycasting
+{
+    public class Boundary
+    {
+        public Line Line { get; set; }
+
+        public Boundary(double x1, double y1, double x2, double y2)
+        {
+            Line = new Line()
+            {
+                Stroke = Brushes.White,
+                StrokeThickness = 1,
+                X1 = x1,
+                Y1 = y1,
+                X2 = x2,
+                Y2 = y2
+            };
+        }
+    }
+}
--- Raycasting/MainWindow.xaml.cs
+++ Raycasting/MainWindow.xaml.cs
@@ -15,14 +15,47 @@ using System.Windows.Shapes;
 
 namespace Raycasting
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        
+        private Random _random = new Random();
+
+        private List<Boundary> _boundaryList = new List<Boundary>();
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoundaries();
+
+            MouseMove += Playground_MouseMove;
+        }
+
+        private void InitializeBoundaries()
+        {
+            int amountBoundaries = 1;
+
+            for (int i = 0; i < amountBoundaries; i++)
+            {
+                double randomX1 = _random.Next(1, 1000);
+                double randomY1 = _random.Next(1, 1000);
+                double randomX2 = _random.Next(1, 1000);
+                double randomY2 = _random.Next(1, 1000);
+
+                _boundaryList.Add(new Boundary(randomX1, randomY1, randomX2, randomY2));
+            }
+
+
+            //Borderlines
+            //_boundaryList.Add(new Boundary(0, 0, 1000, 0)); //Top line
+            //_boundaryList.Add(new Boundary(0, 0, 0, 1000)); //Left line
+            //_boundaryList.Add(new Boundary(1000, 0, 1000, 1000)); //Right line
+            //_boundaryList.Add(new Boundary(0, 1000, 1000, 1000)); //Bottom line
+        }
+
+        private void Playground_MouseMove(object sender, MouseEventArgs e)
+        {
+            Vector mousePos = (Vector)e.GetPosition(Playground);
+            //--
         }
     }
 }
